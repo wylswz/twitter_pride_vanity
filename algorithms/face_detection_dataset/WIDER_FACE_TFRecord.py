bb47cd665cb3ea45bfaf5de55b4a04f27bd429f3
yunlu <wylswz@icloud.com>
2019-03-24 15:30:44 +1100

    multithread

--- algorithms/face_detection_dataset/WIDER_FACE_TFRecord.py
+++ algorithms/face_detection_dataset/WIDER_FACE_TFRecord.py
@@ -1,12 +1,9 @@
+# On branch algorithms
+
 import tensorflow as tf
-import re, os, io, codecs
+import re, os, io, codecs, ast
 from object_detection.utils import dataset_util
 from PIL import  Image
-ANNOTATION_PATH = "/home/johnny/RCNN/dataset/wider_face_split/wider_face_train_bbx_gt.txt"
-TRAIN_IMG_ROOT = "/home/johnny/RCNN/dataset/WIDER_train/images"
-TEST_IMG_ROOT = "/home/johnny/RCNN/dataset/WIDER_val/images"
-TEST = "/home/johnny/RCNN/dataset/wider_face_split/test"
-
 flags = tf.app.flags
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
 FLAGS = flags.FLAGS
@@ -19,25 +16,39 @@ before using the code
 Setting PATHONPATH is quite important
 """
 
+NUM_WORKERS = 8
+CHUNK_SIZE = 1000
+from multiprocessing.pool import Pool
+
+
+ANNOTATION_PATH = "/home/johnny/RCNN/dataset/wider_face_split/wider_face_train_bbx_gt.txt"
+TRAIN_IMG_ROOT = "/home/johnny/RCNN/dataset/WIDER_train/images"
+TEST_IMG_ROOT = "/home/johnny/RCNN/dataset/WIDER_val/images"
+TEST = "/home/johnny/RCNN/dataset/wider_face_split/test"
+
 
 def record_gen():
     """
     A generator that yields one TF data sample one time
-    :return:
+    :return:{"file":"{filepath}", "gt":[ymin, xmin, ymax, xmax]}
     """
+    space = r'\s*'
+    instance_counter = 0
     with open(ANNOTATION_PATH) as fp:
         while True:
-            line = fp.readline()
-            if len(line) == 0:
+            name = fp.readline()
+            if len(name) == 0:
+                yield None
                 break
-            re_match = re.search(r'(\.jpg)', str(line))
+            re_match = re.search(r'(\.jpg)', str(name))
             if re_match is not None:
-
+                instance_counter += 1
                 num_samples = int(fp.readline())
-                data_sample = {"file": str(line).replace('\n', ''), "gt": [None]*num_samples}
+                data_sample = {"file": str(name).replace('\n', ''), "gt": [None]*num_samples}
                 for i in range(num_samples):
                     bbox = fp.readline()
-                    bbox = bbox.split(' ')
+                    bbox = bbox.replace('\n', '')
+                    bbox = re.split(' ', bbox)
                     x_min = int(bbox[0])
                     y_min = int(bbox[1])
                     width = int(bbox[2])
@@ -45,20 +56,22 @@ def record_gen():
                     x_max = (x_min + width)
                     y_max = y_min + height
                     data_sample["gt"][i] = [y_min, x_min, y_max, x_max]
+                print(instance_counter)
                 yield data_sample
 
 
-def tf_sample(gen) -> tf.train.Example:
-
-    try:
-        sample = next(gen)
-    except StopIteration:
+def tf_sample(sample) -> tf.train.Example:
+    """
+    Reading data from generator and conver to a tf example
+    :param gen: A generator
+    :return: tf.train.example
+    """
+    if sample is None:
         return None
-
+    print(sample)
     filename = sample['file']
     gts = sample['gt']
-
-    filename = str(os.path.join(TRAIN_IMG_ROOT,filename))
+    filename = os.path.join(TRAIN_IMG_ROOT, filename)
     with open(filename, 'rb') as fp:
         image_fp: Image.Image = Image.open(fp)
         height = image_fp.size[1]
@@ -95,8 +108,33 @@ def tf_sample(gen) -> tf.train.Example:
     return tf_example
 
 
+def schedule(gen):
+    try:
+        while True:
+            param_lsit = [None] * CHUNK_SIZE
+            for pc in range(CHUNK_SIZE):
+                param_lsit[pc] = next(gen)
+
+            pool = Pool(NUM_WORKERS)
+            res = pool.map(tf_sample, param_lsit)
+            pool.close()
+            pool.join()
+
+            #for p in param_lsit:
+            #    tf_sample(p)
+    except StopIteration:
+        pool = Pool(NUM_WORKERS)
+        res = pool.map(tf_sample, param_lsit)
+        pool.close()
+        pool.join()
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
     gen = record_gen()
-    tf_sample(gen)
+    schedule(gen)
