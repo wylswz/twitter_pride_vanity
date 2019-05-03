b2a14e5a26e2936d25880ee6b4d26f65b0ce5b35
yunlu <wylswz@icloud.com>
2019-05-03 15:01:01 +1000

    web siamese wrapper

--- algorithms/web/web_server.py
+++ algorithms/web/web_server.py
@@ -28,43 +28,52 @@ if not os.path.isdir(TEMP_PATH):
     os.makedirs(TEMP_PATH)
 
 
+@app.route('/api/v1/face_comparison', methods=['POST'])
+    if request.method == 'POST':
+
+
 @app.route('/api/v1/face_detection', methods=['POST'])
 def detection():
     if request.method == 'POST':
-        ml_res = {
-            'detection_boxes': np.array([]),
-            'detection_scores': np.array([])
-        }
-        if request.files.get('image_file') is not None:
-            file = request.files['image_file']
-            ml_res = fdw.predict(file)
+        try:
+            ml_res = {
+                'detection_boxes': np.array([]),
+                'detection_scores': np.array([])
+            }
+            if request.files.get('image_file') is not None:
+                file = request.files['image_file']
+                ml_res = fdw.predict(file)
 
-        elif request.form.get('image_url') is not None:
-            file = request.form['image_url']
-            resp = requests.get(file)
-            affix = file.split('.')[-1]
-            temp_filename = str(uuid.uuid4()) + '.' + affix
-            with open(temp_filename, 'wb') as fp:
-                fp.write(resp.content)
-            ml_res = fdw.predict(open(temp_filename, 'rb'))
-            os.remove(temp_filename) 
+            elif request.form.get('image_url') is not None:
+                file = request.form['image_url']
+                resp = requests.get(file)
+                affix = file.split('.')[-1]
+                temp_filename = str(uuid.uuid4()) + '.' + affix
+                with open(temp_filename, 'wb') as fp:
+                    fp.write(resp.content)
+                ml_res = fdw.predict(open(temp_filename, 'rb'))
+                os.remove(temp_filename) 
 
-        limit = request.form.get('limit')
-        faces = ml_res['detection_boxes'].tolist()
-        scores = ml_res['detection_scores'].tolist()
-        if limit is not None:
-            limit = int(limit)
-            faces = faces[:limit]
-            scores = scores[:limit]
+            limit = request.form.get('limit')
+            faces = ml_res['detection_boxes'].tolist()
+            scores = ml_res['detection_scores'].tolist()
+            if limit is not None:
+                limit = int(limit)
+                faces = faces[:limit]
+                scores = scores[:limit]
 
 
-        return jsonify({
-            'faces': faces,
-            'scores': scores,
-            'format': ['ymin', 'xmin', 'ymax', 'xmax'],
-            'version': 'Tensorflow Models Object-Detection @Iteration 4066'
-        })
+            return jsonify({
+                'faces': faces,
 
+                'scores': scores,
+                'format': ['ymin', 'xmin', 'ymax', 'xmax'],
+                'version': 'Tensorflow Models Object-Detection @Iteration 4066'
+            })
+        except Exception as e:
+            return jsonify({
+                'error':str(e)
+            })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
